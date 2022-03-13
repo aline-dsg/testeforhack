6fb76c2abe981a98037e58c9f366e84af34a8c5d
Aline <alinebmthh@gmail.com>
2022-03-13 17:03:25 -0300

    tentando mergir de novo

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ num = int(input('Insira aqui a sua idade: '))
 print("Sua idade é igual a", num)
 
 #Vamos tentar mergir de fato, mas como??
+
+num2 = input("Insira aqui o seu nome: ")
+
+print("Seu nome é igual a", num2)
